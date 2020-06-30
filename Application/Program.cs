d860f5045fbf915892744771c43a05d51ddacd0d
PetyoZh <petyozh@yahoo.com>
2020-06-30 13:37:41 +0200

    init

---
+++ Application/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogUsers
+{
+    using System.Threading;
+
+    using LogTest;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogInterface  logger = new AsyncLogInterface();
+
+            for (int i = 0; i < 15; i++)
+            {
+                logger.WriteLog("Number with Flush: " + i.ToString());
+                Thread.Sleep(50);
+            }
+
+            logger.Stop_With_Flush();
+
+            LogInterface logger2 = new AsyncLogInterface();
+
+            for (int i = 50; i > 0; i--)
+            {
+                logger2.WriteLog("Number with No flush: " + i.ToString());
+                Thread.Sleep(20);
+            }
+
+            logger2.Stop_Without_Flush();
+
+            Console.ReadLine();
+        }
+    }
+}
