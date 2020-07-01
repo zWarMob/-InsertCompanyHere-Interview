2e77639c8f6277cf834032fcd2ade034e2f9271f
PetyoZh <petyozh@yahoo.com>
2020-07-01 19:13:12 +0200

    Add tests for shutdown

--- LogComponentTests/UnitTests.cs
+++ LogComponentTests/UnitTests.cs
@@ -41,13 +41,51 @@ namespace LogComponentTests
         [Test]
         public void FieLogger_FlushShutdown()
         {
-            Assert.Pass();
+            string testContent = "Number with Flush: 1";
+
+            LogLine expectedLine = new LogLine(testContent);
+
+            string expectedContent = expectedLine.ToString().Substring(LogLine.timestampFormat.Length);
+
+            for (int i = 10000; i > 0; i--)
+            {
+                logger.WriteLog("Number with Flush: " + i.ToString());
+            }
+
+            logger.Stop_With_Flush();
+
+            Thread.Sleep(1500);
+
+            var file = (File.OpenText(Path.Combine(logger.LogPath, logger.OpenLogFileName)));
+
+            var content = file.ReadToEnd();
+
+            Assert.IsTrue(content.EndsWith(expectedContent));
         }
 
         [Test]
         public void FileLogger_ForcedShutdown()
         {
-            Assert.Pass();
+            string testContent = "Number with Flush: 1";
+
+            LogLine expectedLine = new LogLine(testContent);
+
+            string expectedContent = expectedLine.ToString().Substring(LogLine.timestampFormat.Length);
+
+            for (int i = 10000; i > 0; i--)
+            {
+                logger.WriteLog("Number with Flush: " + i.ToString());
+            }
+
+            logger.Stop_Without_Flush();
+
+            Thread.Sleep(1500);
+
+            var file = (File.OpenText(Path.Combine(logger.LogPath, logger.OpenLogFileName)));
+
+            var content = file.ReadToEnd();
+
+            Assert.IsFalse(content.EndsWith(expectedContent));
         }
 
         [Test]
