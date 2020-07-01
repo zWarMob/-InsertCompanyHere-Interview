2b5d681b216d8f42913077d29d36e163ee28a7f5
PetyoZh <petyozh@yahoo.com>
2020-07-01 19:06:41 +0200

    Setup unit test to check if logger has written anything to a file

--- LogComponentTests/UnitTests.cs
+++ LogComponentTests/UnitTests.cs
@@ -1,5 +1,8 @@
 using LogComponent;
 using NUnit.Framework;
+using System;
+using System.IO;
+using System.Threading;
 
 namespace LogComponentTests
 {
@@ -16,8 +19,23 @@ namespace LogComponentTests
         [Test]
         public void FieLogger_Log()
         {
-            logger.WriteLog("Test");
-            Assert.Pass();
+            string testContent = "Let's see if this really works";
+
+            LogLine expectedLine = new LogLine(testContent);
+
+            string expectedContent = expectedLine.ToString().Substring(LogLine.timestampFormat.Length);
+
+            logger.WriteLog(testContent);
+
+            logger.Stop_With_Flush();
+
+            Thread.Sleep(1500);
+
+            var file = (File.OpenText(Path.Combine(logger.LogPath, logger.OpenLogFileName)));
+
+            var content = file.ReadToEnd();
+
+            Assert.IsTrue(content.EndsWith(expectedContent));
         }
 
         [Test]
