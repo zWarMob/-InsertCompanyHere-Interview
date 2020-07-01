e9b870adb4fdf771fc4c34f4091ead5e3d99eaeb
PetyoZh <petyozh@yahoo.com>
2020-07-01 18:34:52 +0200

    Use .NET Core instead

---
+++ LogComponentCore/FileLogger.cs
@@ -0,0 +1,93 @@
+﻿namespace LogComponent
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
+
+    public class FileLogger : ILogger
+    {
+        public readonly ConcurrentBag<LogLine> Lines = new ConcurrentBag<LogLine>();
+        public string LogPath = @"C:\LogTest";
+        public string OpenLogFileName;
+
+        private readonly Thread _runThread;
+        private StreamWriter _writer;
+        private int _curDay;
+        private bool _stop;
+        private bool _forceStop;
+
+
+        private void MainLoop()
+        {
+            while (!this._stop)
+            {
+                Thread.Sleep(1000); //check every second for any new lines, if we have finished logging
+
+                if (_curDay != DateTime.Now.Day)
+                {
+                    CreateNewStreamWriter();
+                }
+
+                if (this.Lines.Any())
+                {
+                    while (this.Lines.TryTake(out LogLine logLine))
+                    {
+                        if (_forceStop)
+                            return;
+
+                        this._writer.Write(logLine);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Create a new file with the current timestamp. _writer references the new stream
+        /// </summary>
+        private void CreateNewStreamWriter()
+        {
+            _curDay = DateTime.Now.Day;
+
+            var OpenLogFileName = @"Log" + DateTime.Now.ToString("yyyyMMdd HHmmss fff") + ".log";
+
+            this._writer = File.AppendText(Path.Combine(LogPath, OpenLogFileName));
+
+            this._writer.Write("Timestamp".PadRight(LogLine.timestampFormat.Length, ' ') + "\t" + "Data" + Environment.NewLine);
+
+            this._writer.AutoFlush = true;
+        }
+
+        public FileLogger()
+        {
+            if (!Directory.Exists(LogPath))
+                Directory.CreateDirectory(LogPath);
+
+            CreateNewStreamWriter();
+
+            this._runThread = new Thread(this.MainLoop);
+            this._runThread.Start();
+        }
+
+        public void Stop_Without_Flush()
+        {
+            this._forceStop = true;
+        }
+
+        public void Stop_With_Flush(object sender, EventArgs e)
+        {
+            Stop_With_Flush();
+        }
+
+        public void Stop_With_Flush()
+        {
+            this._stop = true;
+        }
+
+        public void WriteLog(string s)
+        {
+            this.Lines.Add(new LogLine(s));
+        }
+    }
+}
---
+++ LogComponentCore/LogInterface.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogComponent
+{
+    public interface ILogger
+    {
+        /// <summary>
+        /// Stop the logging. If any outstadning logs theses will not be written to Log
+        /// </summary>
+        void Stop_Without_Flush();
+
+        /// <summary>
+        /// Stop the logging. The call will not return until all all logs have been written to Log.
+        /// </summary>
+        void Stop_With_Flush();
+
+        /// <summary>
+        /// Stop the logging. The call will not return until all all logs have been written to Log.
+        /// </summary>
+        void Stop_With_Flush(object sender, EventArgs e);
+
+        /// <summary>
+        /// WriteLog a message to the Log.
+        /// </summary>
+        /// <param name="s">The s to written to the log</param>
+        void WriteLog(string s);
+
+
+    }
+}
